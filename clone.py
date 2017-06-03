00de785fc4abd970e64dd035296fc1156a2d6220
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2017-06-02 23:29:43 -0500

    added cropping to images

--- clone.py
+++ clone.py
@@ -49,9 +49,10 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Lambda
+from keras.layers.core import Flatten, Dense, Lambda, Cropping2D
 
 model = Sequential()
+model.add(Cropping2D(cropping=((80, 25), (0,0))))
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Flatten())
 model.add(Dense(1))
