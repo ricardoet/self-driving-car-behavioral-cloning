c00d5add44437a3efecc318356f7543f4d147791
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2017-06-03 22:11:55 -0500

    error on 30% logic

--- clone.py
+++ clone.py
@@ -11,7 +11,7 @@ with open('Training_data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-
+	ignore = True
 	center_image_path = line[0]
 	center_image_filename = center_image_path.split('\\')[-1]
 	center_image_full_path = 'Training_data/IMG/' + center_image_filename
@@ -21,24 +21,27 @@ for line in lines:
 	if measurement == float(0):
 		rand_num = np.random.randint(10)
 		if rand_num <= 3: #30% chance of staying if it's a 0
-			measurements.append(measurement)
+			ignore = False
 
-			left_image_path = line[1]
-			left_image_filename = left_image_path.split('\\')[-1]
-			left_image_full_path = 'Training_data/IMG/' + left_image_filename
-			left_image = cv2.imread(left_image_full_path)
-			images.append(left_image)
-			measurement = float(line[3]) + 0.08
-			measurements.append(measurement)
+	if not ignore:
+		measurements.append(measurement)
 
-			right_image_path = line[2]
-			right_image_filename = right_image_path.split('\\')[-1]
-			#print(right_image_filename)
-			right_image_full_path = 'Training_data/IMG/' + right_image_filename
-			right_image = cv2.imread(right_image_full_path)
-			images.append(right_image)
-			measurement = float(line[3]) - 0.15
-			measurements.append(measurement)
+		left_image_path = line[1]
+		left_image_filename = left_image_path.split('\\')[-1]
+		left_image_full_path = 'Training_data/IMG/' + left_image_filename
+		left_image = cv2.imread(left_image_full_path)
+		images.append(left_image)
+		measurement = float(line[3]) + 0.08
+		measurements.append(measurement)
+
+		right_image_path = line[2]
+		right_image_filename = right_image_path.split('\\')[-1]
+		#print(right_image_filename)
+		right_image_full_path = 'Training_data/IMG/' + right_image_filename
+		right_image = cv2.imread(right_image_full_path)
+		images.append(right_image)
+		measurement = float(line[3]) - 0.15
+		measurements.append(measurement)
 
 
 augmented_images, augmented_measurements = [], []
