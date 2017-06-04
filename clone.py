87438e63554ee09025802f7ab4a7a337b7a81858
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2017-06-03 21:54:27 -0500

    added new training data and function to delete 70% of 0 steering images

--- clone.py
+++ clone.py
@@ -18,24 +18,27 @@ for line in lines:
 	center_image = cv2.imread(center_image_full_path)
 	images.append(center_image)
 	measurement = float(line[3])
-	measurements.append(measurement)
+	if measurement == float(0):
+		rand_num = np.random.randint(10)
+		if rand_num <= 3: #30% chance of staying if it's a 0
+			measurements.append(measurement)
 
-	left_image_path = line[1]
-	left_image_filename = left_image_path.split('\\')[-1]
-	left_image_full_path = 'Training_data/IMG/' + left_image_filename
-	left_image = cv2.imread(left_image_full_path)
-	images.append(left_image)
-	measurement = float(line[3]) + 0.08
-	measurements.append(measurement)
+			left_image_path = line[1]
+			left_image_filename = left_image_path.split('\\')[-1]
+			left_image_full_path = 'Training_data/IMG/' + left_image_filename
+			left_image = cv2.imread(left_image_full_path)
+			images.append(left_image)
+			measurement = float(line[3]) + 0.08
+			measurements.append(measurement)
 
-	right_image_path = line[2]
-	right_image_filename = right_image_path.split('\\')[-1]
-	#print(right_image_filename)
-	right_image_full_path = 'Training_data/IMG/' + right_image_filename
-	right_image = cv2.imread(right_image_full_path)
-	images.append(right_image)
-	measurement = float(line[3]) - 0.15
-	measurements.append(measurement)
+			right_image_path = line[2]
+			right_image_filename = right_image_path.split('\\')[-1]
+			#print(right_image_filename)
+			right_image_full_path = 'Training_data/IMG/' + right_image_filename
+			right_image = cv2.imread(right_image_full_path)
+			images.append(right_image)
+			measurement = float(line[3]) - 0.15
+			measurements.append(measurement)
 
 
 augmented_images, augmented_measurements = [], []
