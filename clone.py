b1f76cdca1fd65636de6c29e75473572345fe838
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2017-06-06 13:27:38 -0500

    I don't know why this isn't working. Back to Udacity's data and randomized the image flipping

--- clone.py
+++ clone.py
@@ -12,8 +12,15 @@ def cropAndResize(image):
 
 	return scipy.misc.imresize(image, (64, 64))
 
+def randomize(probability=5):
+	random = randint(0,9)
+	if random < probability:
+		return True	
+	else:
+		return False
+
 lines = []
-with open('Training_data/driving_log.csv') as csvfile:
+with open('Udacity_data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -24,11 +31,11 @@ for line in lines:
 
 	valid = True
 	center_image_path = line[0]
-	center_image_filename = center_image_path.split('\\')[-1]
-	center_image_full_path = 'Training_data/IMG/' + center_image_filename
+	center_image_filename = center_image_path.split('/')[-1]
+	center_image_full_path = 'Udacity_data/IMG/' + center_image_filename
 	center_image = plt.imread(center_image_full_path)
 	measurement = float(line[3])
-	if measurement == float(0):
+	if measurement == abs(float(0)) < 0.5:
 		random = randint(0,9)
 		if random >= 7:
 			measurements.append(measurement)
@@ -37,8 +44,8 @@ for line in lines:
 	random = randint(0,9)
 	if random <= 5:
 		left_image_path = line[1]
-		left_image_filename = left_image_path.split('\\')[-1]
-		left_image_full_path = 'Training_data/IMG/' + left_image_filename
+		left_image_filename = left_image_path.split('/')[-1]
+		left_image_full_path = 'Udacity_data/IMG/' + left_image_filename
 		left_image = plt.imread(left_image_full_path)
 		images.append(cropAndResize(left_image))
 		measurement = float(line[3]) + 0.25
@@ -47,9 +54,9 @@ for line in lines:
 	random = randint(0,9)
 	if random <= 5:
 		right_image_path = line[2]
-		right_image_filename = right_image_path.split('\\')[-1]
+		right_image_filename = right_image_path.split('/')[-1]
 		#print(right_image_filename)
-		right_image_full_path = 'Training_data/IMG/' + right_image_filename
+		right_image_full_path = 'Udacity_data/IMG/' + right_image_filename
 		right_image = plt.imread(right_image_full_path)
 		images.append(cropAndResize(right_image))
 		measurement = float(line[3]) - 0.25
@@ -58,10 +65,11 @@ for line in lines:
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
-	augmented_images.append(image)
-	augmented_measurements.append(measurement)
-	augmented_images.append(np.fliplr(image))
-	augmented_measurements.append(measurement*-1.0)
+	if randomize():
+		augmented_images.append(image)
+		augmented_measurements.append(measurement)
+		augmented_images.append(np.fliplr(image))
+		augmented_measurements.append(measurement*-1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
